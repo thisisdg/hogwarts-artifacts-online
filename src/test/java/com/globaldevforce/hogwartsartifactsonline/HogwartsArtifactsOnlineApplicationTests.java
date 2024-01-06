cd0f547700789dd29462c2de860d1f9ad50d9a9d
Dhruv Goel <thisisdg@rediffmail.com>
2024-01-06 19:42:28 +0530

    Initial commit

---
+++ src/test/java/com/globaldevforce/hogwartsartifactsonline/HogwartsArtifactsOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globaldevforce.hogwartsartifactsonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HogwartsArtifactsOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
